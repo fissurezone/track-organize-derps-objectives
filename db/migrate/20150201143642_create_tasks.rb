22a1a4df02ab233d2205f0600294ffc1242e43df
Kunal Chowdhury <kunal.chowdhury@teralytics.net>
2015-02-02 00:10:56 +0800

    Added home page

---
+++ db/migrate/20150201143642_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    drop_table :tasks
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.string :status
+
+      t.timestamps null: false
+    end
+  end
+end
