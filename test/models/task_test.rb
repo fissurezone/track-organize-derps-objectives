10163aafd8c2aa433f97468058a616932f7ff596
Kunal Chowdhury <kunal.chowdhury@teralytics.net>
2015-02-05 02:37:15 +0800

    Added nominal tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create task without statusi specified sets status to new" do
+    task = Task.new(title: "Test", details: "Test details")
+    assert task.save
+    assert_equal "New", task.status, "Unspecified status was set to 'New'"
+  end
+
+  test "update status" do
+    [ "Done", "Redo" ].each do |status|
+      task = tasks(:new)
+      task.update(status: status)
+      assert_equal status, task.status
+    end
+  end
 end
